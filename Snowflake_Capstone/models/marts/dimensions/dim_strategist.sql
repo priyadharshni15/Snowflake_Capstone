05befd8986be2c696dcc51319d9576f08b195c0f
Priya Dharshni R <priyadharshni.r@latentview.com>
2026-02-26 16:43:00 +0000

    splited model into dim strategist

--- Snowflake_Capstone/models/marts/dimensions/dim_strategist.sql
+++ Snowflake_Capstone/models/marts/dimensions/dim_strategist.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 select
-    model_id,
-    model_name,
     strategist_id,
+    strategist_code,
+    strategist_name,
     {{ audit_columns() }}
 from {{ ref('stg_model') }}
