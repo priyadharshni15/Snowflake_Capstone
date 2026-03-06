8d808c6b353c115d5947cf2d9d2d1c1fbfacb20c
Priya Dharshni R <priyadharshni.r@latentview.com>
2026-03-06 09:05:44 +0000

    updated dim table logics

--- Snowflake_Capstone/models/marts/dimensions/dim_account_holdings.sql
+++ Snowflake_Capstone/models/marts/dimensions/dim_account_holdings.sql
@@ -1,23 +1,12 @@
-{{ config(
-    materialized='table'
-) }}
-
 select
     accountid,
     ticker,
-    max(modelid) as modelid,
-    max(advisorid) as advisorid,
-    max(bdid) as bdid,
-    max(custodian) as custodian,
-    max(strategistid) as strategistid,
-    max(strategistcode) as strategistcode,
-    max(strategistname) as strategistname,
-    max(marketvalue) as marketvalue,
-    max(description) as description,
-    {{ aggregated_ticker_shares('shares') }} as total_shares
-
+    modelid,
+    advisorid,
+    sum(shares) as total_shares
 from {{ ref('dim_combined_data') }}
-
 group by
     accountid,
-    ticker
+    ticker,
+    modelid,
+    advisorid
