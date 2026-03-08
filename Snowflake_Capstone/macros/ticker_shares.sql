6367fee21fe18214b6afe5b4410819361475c699
Priya Dharshni R <priyadharshni.r@latentview.com>
2026-03-08 16:42:50 +0000

    alignment

--- Snowflake_Capstone/macros/ticker_shares.sql
+++ Snowflake_Capstone/macros/ticker_shares.sql
@@ -1,10 +1,8 @@
 {% macro aggregated_ticker_shares(relation_name) %}
-
 select
     accountid,
     sum(marketvalue) as Total_AUM
 from {{ relation_name }}
 group by
     accountid
-
 {% endmacro %}
